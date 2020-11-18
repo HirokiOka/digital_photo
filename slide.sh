bc1dcd779a15a45f3cb22fc6c3446356244f9700
hiroki <hiro_bass.0827@icloud.com>
2020-11-18 13:59:04 +0900

    fix error

--- slide.sh
+++ slide.sh
@@ -1,3 +1,3 @@
 # git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
-sudo /home/pi/Dropbox-Uploader/dropbox_uploader.sh -s download /home/pi/画像/slide
+sudo /home/pi/Dropbox-Uploader/dropbox_uploader.sh -s download / /home/pi/画像/slide
 sudo feh -Y -x -q -D 5 -R 43200 -F  -r /home/pi/画像/slide
